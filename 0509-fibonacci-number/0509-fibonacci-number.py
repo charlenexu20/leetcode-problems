16d95c63065cfcf218bce0eccaa626149f10f419
Charlene Xu <charlene.xu92@gmail.com>
2024-01-31 15:56:24 -0800

    Time: 35 ms (76.47%), Space: 16.6 MB (60.65%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,12 +1,15 @@
 class Solution:
     def fib(self, n: int) -> int:
-        # BASE CASE
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        
-        # RECURSIVE CASE
-        return self.fib(n - 1) + self.fib(n - 2)
+        # Base case
+        if n < 2:
+            return n
 
-        
+        # Initialize the dp array
+        dp = [0, 1]
+
+        for _ in range(2, n + 1):
+            curr = dp[0] + dp[1]
+            dp[0] = dp[1]
+            dp[1] = curr
+
+        return dp[1] 
