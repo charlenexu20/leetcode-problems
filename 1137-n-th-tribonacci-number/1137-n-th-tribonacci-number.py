ad502cad1a8b242cbf62d2889d89f3714a6c0da0
Charlene Xu <charlene.xu92@gmail.com>
2024-01-31 16:13:49 -0800

    Time: 33 ms (77.65%), Space: 16.4 MB (71.12%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp = [0, 1, 1]
+
+        if n < 3:
+            return dp[n]
+
+        for _ in range(3, n + 1):
+            curr = dp[0] + dp[1] + dp[2]
+            dp[0], dp[1], dp[2] = dp[1], dp[2], curr
+
+        return dp[2]
+
+        
