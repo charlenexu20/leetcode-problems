6f0e543b55c51ed58c7296e67e83d44f891855a1
Charlene Xu <charlene.xu92@gmail.com>
2024-03-18 12:17:58 -0700

    Time: 46 ms (7.94%), Space: 16.5 MB (41.14%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,31 +1,16 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        # hashset | tc: O(logn) | sc: O(logn)
-
-        # keep track of visited numbers to detect cycles
+        """
+        converts the number to a string, iterates over its digits, 
+        and computes the sum of squares.
+        """
         visited = set()
 
-        # loop until encounter 1 or enter a cycle
         while n not in visited:
             visited.add(n)
-            n = self.sum_of_squares(n)
+            n = sum((int(digit) ** 2) for digit in str(n))
 
             if n == 1:
                 return True
 
         return False
-
-    def sum_of_squares(self, n: int) -> int:
-        total = 0
-        
-        # loop until n becomes 0
-        while n:
-            # extract the last digit of n
-            digit = n % 10
-            digit = digit ** 2
-            total += digit
-            # remove the last digit from n
-            n = n // 10
-
-        return total
-        
