d63a6c7c3e859e6d920ace0df0f20c5fc3ceb75f
Charlene Xu <charlene.xu92@gmail.com>
2024-03-26 13:03:29 -0700

    Time: 26 ms (96.92%), Space: 16.5 MB (43.43%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,15 +1,39 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        # stack(optimized) | tc: O(n + m) | sc: O(n + m)
+        # two-pointers(optimized) | tc: O(n + m) | sc: O(1)
 
-        return self.remove_backspace(s) == self.remove_backspace(t)
+        index_s = len(s) - 1
+        index_t = len(t) - 1
 
-    def remove_backspace(self, s):
-        stack = []
-        for char in s:
-            if char != "#":
-                stack.append(char)
-            elif stack:
-                stack.pop()
+        # Iterate through both strings from the end
+        while index_s >= 0 or index_t >=0:
+            # Find the index of next valid character in each string
+            index_s = self.next_valid_index(s, index_s)
+            index_t = self.next_valid_index(t, index_t)
 
-        return "".join(stack)
+            char_s = s[index_s] if index_s >= 0 else ""
+            char_t = t[index_t] if index_t >= 0 else ""
+
+            if char_s != char_t:
+                return False
+            index_s -= 1
+            index_t -= 1
+
+        return True
+
+    # Helper function to skip backspaces
+    def next_valid_index(self, string, index):
+        backspace = 0
+        while index >= 0:
+            if backspace == 0 and string[index] != "#":
+                break
+            elif string[index] == "#":
+                backspace += 1
+            else:
+                backspace -= 1
+            index -= 1
+        return index
+
+
+
+        
