da5ccd18ef254f5208068b821c7e5a9f67662b17
Charlene Xu <charlene.xu92@gmail.com>
2024-04-04 16:26:46 -0700

    Time: 911 ms (38.96%), Space: 32 MB (27.44%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,21 +1,19 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # Intuition (two pointers) | tc: O(n) | sc: O(n)
+        # Monotonic decreasing stack | tc: O(n) | sc: O(n)
         
         res =[0] * len(temperatures)
-        l, r = 0, 1
+        stack = [] # pair: [temp, index]
 
-        while r < len(temperatures):
-            # If temperature at r is warmer than at l
-            if temperatures[r] > temperatures[l]:
-                # update the result for index l
-                res[l] = r - l
-                l, r = l + 1, l + 1
-            else:
-                r += 1
-                # Update pointers if r reaches end
-                if r >= len(temperatures):
-                    l, r = l + 1, l + 1
+        for idx, temp in enumerate(temperatures):
+            # Check if stack is not empty and current temp is warmer than the temp at the top of the stack
+            while stack and temp > stack[-1][0]:
+                # Pop temperature and index from stack
+                stack_temp, stack_idx = stack.pop()
+                # Calculate days until warmer temperature
+                res[stack_idx] = idx - stack_idx
+            # Append current temperature and index pair to stack
+            stack.append([temp, idx])
 
         return res
         
