6dd1c753119769b11139f8f44be33133d5001d09
Charlene Xu <charlene.xu92@gmail.com>
2024-04-23 15:08:36 -0700

    Time: 45 ms (8.17%), Space: 16.7 MB (16.7%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,18 +6,27 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        # DFS recursive | tc: O(n) | sc: O(h)
-        
-        return self.compare(root.left, root.right)
+        # Iterative(queue) | tc: O(n) | sc: O(n)
 
-    
-    def compare(self, left, right):
-        # elminate null and different vals
-        if not left and not right: return True
-        elif not left or not right: return False
-        elif left.val != right.val: return False
+        queue = collections.deque()
+        queue.append(root.left)
+        queue.append(root.right)
 
-        outside = self.compare(left.left, right.right)
-        inside = self.compare(left.right, right.left)
-        return outside and inside
+        while queue:
+            left_node = queue.popleft()
+            right_node = queue.popleft()
+
+            if not left_node and not right_node:
+                continue
+
+            if (not left_node or not right_node) or \
+                (left_node.val != right_node.val):
+                return False
+
+            queue.append(left_node.left)
+            queue.append(right_node.right)
+            queue.append(left_node.right)
+            queue.append(right_node.left)
+
+        return True
 
