4ec89f6772c6dc66bba24a9219ec9c627c6e03f6
Charlene Xu <charlene.xu92@gmail.com>
2024-04-23 15:53:41 -0700

    Time: 40 ms (21.11%), Space: 16.6 MB (10.17%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,13 +6,30 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # DFS | tc: O(n) | sc: O(h)
+        # BFS | tc: O(n) | sc: O(h)
+        if not p and not q: return True
+        if not p or not q: return False
 
-        if not p and not q:
-            return True
-        if not p or not q or p.val != q.val:
-            return False
+        # Combine the queues into one queue of tuples (p_node, q_node)
+        queue = collections.deque([(p, q)])
 
-        # Recursively check if the left and right subtrees are the same
-        return (self.isSameTree(p.left, q.left) and 
-                self.isSameTree(p.right, q.right))
+        while queue:
+            node1, node2 = queue.popleft()
+            
+            if node1.val != node2.val:
+                return False
+
+            # Check the left children
+            if node1.left and node2.left:
+                queue.append((node1.left, node2.left))
+            elif node1.left or node2.left:
+                return False
+
+            # Check the right children
+            if node1.right and node2.right:
+                queue.append((node1.right, node2.right))
+            elif node1.right or node2.right:
+                return False
+
+        return True
+
